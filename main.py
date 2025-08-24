c591bfe30834dc3ad8e0c2d1a6a947a43dfe68d3
jupi999 <65447217+jupi999@users.noreply.github.com>
2025-08-24 08:11:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
